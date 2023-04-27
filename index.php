6e7749fc3d94d6edbe999020c78abc521535c8d4
Melissa Maria Rummer <mrummer@schueler.hakzell.at>
2023-04-27 12:09:08 +0200

    index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Perasser\BirdCounting;
+require 'vendor/autoload.php';
+$bird1 = new Bird(4, "Meise");
+$bird2 = new Bird(3,"Sperlinge");
+
+$countingBirds = new Counting();
+$countingBirds->addBird($bird1);
+$countingBirds->addBird($bird2);
+
+echo $bird1->getName();
+echo $bird2->getName();
+echo "Es sind " . $bird1->getAmount() + $bird2->getAmount() . " Vögel vorhanden.";
