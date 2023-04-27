1f798d0a80843d016dc18bb7412fa89cd82f8390
Melissa Maria Rummer <mrummer@schueler.hakzell.at>
2023-04-27 12:16:41 +0200

    BirdCounter erstellt

--- index.php
+++ index.php
@@ -11,3 +11,6 @@ $countingBirds->addBird($bird2);
 echo $bird1->getName();
 echo $bird2->getName();
 echo "Es sind " . $bird1->getAmount() + $bird2->getAmount() . " Vögel vorhanden.";
+
+$countbird = new BirdCounter($bird1, $bird2);
+echo $countbird->countBirds();
